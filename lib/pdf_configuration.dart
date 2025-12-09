f741088a6b8b8d239f7cb502c8d5143ff3cae32d
Hong Daly <62000319+hongda1y@users.noreply.github.com>
2025-12-09 17:07:53 +0700

    Add PdfMargins class and update PdfConfiguration

--- lib/pdf_configuration.dart
+++ lib/pdf_configuration.dart
@@ -1,27 +1,53 @@
 import 'pdf_configuration_enums.dart';
 
+/// Represents PDF page margins in points.
+class PdfMargins {
+  final double top;
+  final double left;
+  final double bottom;
+  final double right;
+
+  const PdfMargins({
+    this.top = 40.0,
+    this.left = 20.0,
+    this.bottom = 40.0,
+    this.right = 20.0,
+  });
+
+  /// Convenience constructor for uniform vertical margins
+  const PdfMargins.symmetric({double vertical = 40.0, double horizontal = 20.0})
+      : top = vertical,
+        bottom = vertical,
+        left = horizontal,
+        right = horizontal;
+}
+
 class PdfConfiguration {
   final String targetDirectory;
   final String targetName;
   final PrintSize printSize;
   final PrintOrientation printOrientation;
-  /// Make links clickable on iOS. But, any image will need to be set as data base64  <img src="data:image/jpeg;base64,[DATA]" />
+  /// Make links clickable on iOS. But images must be base64 encoded
   final bool linksClickable;
-  /// `targetDirectory` is the desired path for the Pdf file.
+  /// PDF page margins
+  final PdfMargins margins;
+
+  /// `targetDirectory` is the desired path for the PDF file.
   ///
-  /// `targetName` is the name of the Pdf file
+  /// `targetName` is the name of the PDF file.
   ///
-  /// `printSize` is the print size of the Pdf file
+  /// `printSize` is the print size of the PDF file.
   ///
-  /// `printOrientation` is the print orientation of the Pdf file
+  /// `printOrientation` is the print orientation of the PDF file.
   PdfConfiguration({
     required this.targetDirectory,
     required this.targetName,
     this.printSize = PrintSize.A4,
     this.printOrientation = PrintOrientation.Portrait,
     this.linksClickable = false,
+    this.margins = const PdfMargins(), // default A4-like margins
   });
 
-  /// Returns the final path for temporary Html File
+  /// Returns the final path for temporary HTML file
   String get htmlFilePath => "$targetDirectory/$targetName.html";
 }
